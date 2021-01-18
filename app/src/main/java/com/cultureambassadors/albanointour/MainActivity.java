20e766f78cdeb90528e02b38968e1365ce1ecb76
Tox46 <profiloverugby@gmail.com>
2021-01-18 12:54:45 +0100

    Primo layout della home

--- app/src/main/java/com/cultureambassadors/albanointour/MainActivity.java
+++ app/src/main/java/com/cultureambassadors/albanointour/MainActivity.java
@@ -1,8 +1,13 @@
 package com.cultureambassadors.albanointour;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationBuilderWithBuilderAccessor;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+//TODO aggiungere il logo
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startList(View view){
+        Intent intent = new Intent(this, BCActivity.class);
+        String []filters = getResources().getStringArray(R.array.filters);
+        String filter = "";
+        if(view.getId() == R.id.btnReligiosi)
+            intent.putExtra("filter", filters[0]);
+        else if(view.getId() == R.id.btnStorici)
+            intent.putExtra("filter", filters[1]);
+        else if(view.getId() == R.id.btnMostre)
+            intent.putExtra("filter", filters[2]);
+        else if(view.getId() == R.id.btnMusei)
+            intent.putExtra("filter", filters[3]);
+        else if(view.getId() == R.id.btnEventiCulturali)
+            intent.putExtra("filter", filters[4]);
+        else
+            intent.putExtra("filter", filters[5]);
+        startActivity(intent);
+
     }
 }
