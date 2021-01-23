fb270280547ed123c5607f148096a5c4aead5f21
Tox46 <profiloverugby@gmail.com>
2021-01-23 21:33:15 +0100

    Cambiato layout home screen

--- app/src/main/java/com/cultureambassadors/albanointour/MainActivity.java
+++ app/src/main/java/com/cultureambassadors/albanointour/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.cultureambassadors.albanointour.BCListActivity;
 import com.cultureambassadors.albanointour.R;
@@ -14,25 +15,29 @@ public class MainActivity extends AppCompatActivity {
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //TODO aggiungere splashscreen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.mainToolbar);
+        toolbar.setTitle(R.string.app_name);
+
     }
     
     public void startList(View view){
         Intent intent = new Intent(this, BCListActivity.class);
         String []filters = getResources().getStringArray(R.array.filters);
         String filter = "";
-        if(view.getId() == R.id.btnReligiosi)
+        if(view.getId() == R.id.mainCardEdificiReligiosi)
             intent.putExtra("filter", filters[0]);
-        else if(view.getId() == R.id.btnStorici)
+        else if(view.getId() == R.id.mainCardEdificiStorici)
             intent.putExtra("filter", filters[1]);
-        else if(view.getId() == R.id.btnMostre)
+        else if(view.getId() == R.id.mainCardMostre)
             intent.putExtra("filter", filters[2]);
-        else if(view.getId() == R.id.btnMusei)
+        else if(view.getId() == R.id.mainCardMusei)
             intent.putExtra("filter", filters[3]);
-        else if(view.getId() == R.id.btnEventiCulturali)
+        else if(view.getId() == R.id.mainCardEventiCulturali)
             intent.putExtra("filter", filters[4]);
-        else
+        else if(view.getId() == R.id.mainCardParchi)
             intent.putExtra("filter", filters[5]);
         startActivity(intent);
         
