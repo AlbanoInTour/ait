182e6b038f5f542c5782dc7863ce19a80af55204
Tox46 <profiloverugby@gmail.com>
2021-01-23 22:13:17 +0100

    Aggiunta Splash Screen

--- app/src/main/java/com/cultureambassadors/albanointour/MainActivity.java
+++ app/src/main/java/com/cultureambassadors/albanointour/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //TODO aggiungere splashscreen
+        setTheme(R.style.Theme_AlbanoInTour);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.mainToolbar);
